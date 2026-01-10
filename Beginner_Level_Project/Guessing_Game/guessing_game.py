37951b8b30d70479d13bc1cb816c71470373a61f
Beta <118555211+nullbeing72@users.noreply.github.com>
2026-01-10 20:01:04 +0530

    Update guessing_game.py

--- Beginner_Level_Project/Guessing_Game/guessing_game.py
+++ Beginner_Level_Project/Guessing_Game/guessing_game.py
@@ -1,12 +1,16 @@
+# importing the 'random' module to generate a random number
 import random
 
+# displaying game title and introduction
 print("--NUMBER GUESSING GAME--")
 print("Welcome to the guessing game")
 print("I am thinking of a number between 1 and 100")
-print("And, my dear friend, You have to guess the number.", end='\n\n')
+print("And, my dear friend, you have to guess the number.", end='\n\n')
 print("Are you ready?")
 
-signal = False
+signal = False # Flag to check whether the user is ready to play
+
+# loop to ask the user if they are ready (maximum 4 attempts)
 for attempts in range(0, 4):
     ready = input("[Y]es or [N]o: ").lower()
     if ready == "y":
@@ -18,18 +22,21 @@ for attempts in range(0, 4):
     else:
         print("User input is not valid.")
 
-
+# if user agreed to play
 if signal:
-    n_guess = 1
-    invalid_guess_attempts = 0
-    generated = random.randint(1, 100)
-    print("I have guessed the number!")
+    n_guess = 1 # counter to tract the number of valid guesses
+    invalid_guess_attempts = 0 # counter for invalid guess attempts
+    generated = random.randint(1, 100) # generating random numbers between 1 and 100
+    print("I have guessed the number!") 
     print("Your turn!", end="\n\n")
 
+    # infinite loop until the user guesses correctly or exits
     while True:
         try:
+            # taking the user's guess
             guess = int(input("Enter your guess: "))
         except ValueError:
+            # handling non-integer input
             invalid_guess_attempts += 1
             print("Please enter a valid number. An integer in the range from 1 to 100.\n")
             if invalid_guess_attempts > 3:
@@ -38,6 +45,7 @@ if signal:
                 break
             continue
 
+        # checking if the number is outside the valid range
         if guess > 100 or guess < 1:
             invalid_guess_attempts += 1
             print("Please enter a valid number. An integer in the range from 1 to 100.\n")
@@ -46,7 +54,10 @@ if signal:
                 print("Exiting the game...")
                 break
         else:
+            # reset invalid attempts 
             invalid_guess_attempts = 0
+
+            # if user guessed the correct number
             if guess == generated:
                 print("\033[1m\nHurray! You guessed the number!\033[0m")
                 print(f"#guess: {n_guess}\n")
@@ -59,13 +70,15 @@ if signal:
                 print("Exiting the game...")
                 break
             else:
+                # guess handling if the guess is not right
                 print(f"Guess {n_guess}")
                 if guess > generated:
                     print("HINT: Too high\n")
                 else:
                     print("HINT: Too low\n")
-                n_guess += 1
+                n_guess += 1 # increment guess count
 
+# if user never agreed to play or just messing around
 else:
     print("Too many attempts!")
     print("Exiting the game...")
